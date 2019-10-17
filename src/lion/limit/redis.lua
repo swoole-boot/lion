b0df15af4b633e474ecc5262bacfcd10649f14d9
Jiang Haiqiang <jhq0113@163.com>
2019-10-17 14:42:53 +0800

    update traffic limit

--- src/lion/limit/redis.lua
+++ src/lion/limit/redis.lua
@@ -10,24 +10,31 @@ local ext    = require "lion.extension"
 ---获取token的redis lua脚本
 ---
 local tokenScript = [[
+        --限速key
         local key         = ARGV[1]
-        local limit       = ARGV[2]
-        local microTimeId = ARGV[3]
-        local zone        = ARGV[4]
 
-        zone        = zone or 60
-        limit       = tonumber(limit)
-        local time  = tonumber(string.sub(microTimeId, 1, 10))
+        --限速阈值
+        local limit       = ARGV[2]
+        limit             = tonumber(limit)
+
+        --要求前十个字节是unix时间戳
+        local microTimeId = ARGV[3]
+        local time        = tonumber(string.sub(microTimeId, 1, 10))
+
+        --时间区间
+        local zone        = tonumber(ARGV[4])
 
         local length = redis.call("LLEN", key)
         if length < limit then
-            length = redis.call("LPUSH", key, microTimeId)
+            redis.call("LPUSH", key, microTimeId)
+            redis.call("EXPIRE", key, zone + 1)
             return 1
         else
-            local endId         = redis.call("LINDEX", key, limit - 1)
+            local endId    = redis.call("LINDEX", key, limit - 1)
             local timespan = time - tonumber(string.sub(endId, 1, 10))
-            if timespan >= tonumber(zone) then
+            if timespan >= zone then
                 redis.call("LPUSH", key, microTimeId)
+                redis.call("EXPIRE", key, zone + 1)
                 redis.call("RPOP", key)
                 return 1
             else
@@ -73,16 +80,34 @@ function _M:redis()
     return self.client
 end
 
+---获得限流token
+---@param key   string   关联key
+---@param limit number   限制数量,如果小于1,表示服务降级
+---@param zone  number   单位时间,单位为s,默认为60s
+---@return string
+---
 function _M:token(key, limit, zone)
     local hash, err = _M:redis():script("load", tokenScript)
     if hash == false then
         ngx.log(ngx.ERR,"load tokenScript error:",err)
         return false
     end
-    limit = limit or 10
-    zone  = zone  or 10
-    local result = _M:redis():evalSha(hash, 0, key, limit, ext.uniqueId("",20), zone)
-    return result == 1
+
+    --- 如果个数小于1,表示被降级
+    if limit < 1 then
+        return false
+    end
+
+    zone = zone or 60
+
+    local uniqueId = ext.uniqueId("",20)
+    local result,error = _M:redis():evalSha(hash, 0, key, limit, uniqueId, zone)
+    if result == false then
+        ngx.log(ngx.ERR,"get token error:",error)
+        return false
+    end
+
+    return uniqueId
 end
 
 return _M
